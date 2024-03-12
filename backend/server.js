d8848c612746f4a3fb009deb41bcbf134325afe9
ic3ic3ic3 <ashwanisajwanpro@gmail.com>
2024-03-12 17:17:06 +0530

    final build

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express  from "express";
 import dotenv from "dotenv"
 import cookieParser from 'cookie-parser'
@@ -11,11 +12,9 @@ import { app,server } from "./socket/socket.js"
 
 const PORT = process.env.PORT || 5000
 
+const __dirname= path.resolve()
 dotenv.config()
 
-// app.get('/',(req , res)=>{
-//     res.send("HELLO WORLD")
-// })
 
 app.use(express.json()) //to parse incoming request with json payload
 app.use(cookieParser()) // to read cookies
@@ -25,8 +24,11 @@ app.use("/api/auth/",authRoutes)
 app.use("/api/messages/",messageRoutes)
 app.use("/api/users/",userRoutes)
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
-
+app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 
 
